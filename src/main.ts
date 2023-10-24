01c29e045db33a1f1a0cb74e1c922ef4e177134f
Moises B <mbaez091@gmail.com>
2023-10-23 21:50:17 -0300

    read port from .env, guards

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -15,6 +17,12 @@ async function bootstrap() {
     }),
   );
 
+  const configService = app.get(ConfigService);
+
+  const PORT = configService.get('PORT');
+
+  console.log('main.bootstrap.PORT', PORT);
+
   const config = new DocumentBuilder()
     .setTitle('API')
     .setDescription('PLATZI STORE')
@@ -22,6 +30,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
